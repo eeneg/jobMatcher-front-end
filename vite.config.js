e5e75b7acbf22e39a820389e34ef602020c20a3c
eeneg <generellanos@gmail.com>
2025-08-14 00:54:23 +0800

    vite and package fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jobMatcher-front-end/',
   plugins: [
     react(),
     tailwindcss()
